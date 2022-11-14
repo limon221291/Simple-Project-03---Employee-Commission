948d835b6f67e7a005e5bd6e4a2bdca8abbd6e8f
Kaium Al Limon <116227589+limon221291@users.noreply.github.com>
2022-11-14 21:07:51 +0600

    Update Main.java

--- Project 3/src/Main.java
+++ Project 3/src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         Employee emp1=new Employee();
 
-        emp1.setInfo(9.0,35000.5,110.00);
+        emp1.setInfo(9.0,35000.5,110.00); // User can use custom values as their wish
         emp1.printWage();
     }
 }
